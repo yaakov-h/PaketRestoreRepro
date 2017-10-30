7021af83ac5e219daf760da68c8d91956555e5cf
Yaakov <yaakov.smith@wisetechglobal.com>
2017-10-30 17:07:42 +1100

    Repro case for paket team

---
+++ ClassLibrary1/Class1.cs
@@ -0,0 +1,9 @@
+﻿using Newtonsoft.Json.Linq;
+
+namespace ClassLibrary1
+{
+    public static class Class1
+    {
+        public static string MakeSomeJson() => new JObject().ToString();
+    }
+}
---
+++ ClassLibrary2/Class1.cs
@@ -0,0 +1,10 @@
+﻿using NUnit.Framework;
+
+namespace ClassLibrary2
+{
+    class Class1
+    {
+        [Test]
+        public void MadeSomeJson() => Assert.That(ClassLibrary1.Class1.MakeSomeJson(), Is.Not.Null.Or.Empty);
+    }
+}
